6123a6056de97a34f326eb886f4f77d0539c98e7
calmosowl <calmos.owl@gmail.com>
2018-01-16 08:36:10 +0200

    new

---
+++ libcalm.js
@@ -0,0 +1,19 @@
+let makeElement = function(parent, className, innerHtml, style) {
+
+		let	e = document.createElement('div');
+		e.className = className;
+
+		if (innerHtml) {
+			e.innerHTML = innerHtml;
+		}
+
+		if (style) {
+			for (var prop in style) {
+				e.style[prop] = style[prop];
+			}
+		}
+
+		parent.appendChild(e);
+		
+		return e;
+	};
